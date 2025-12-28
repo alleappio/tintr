65a61c3c667829ff6f6bd440613f02023e35d7a2
aappio <alle.appio@gmail.com>
2025-12-28 20:14:41 +0100

    use color format converters
    Signed-off-by: aappio <alle.appio@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -2,28 +2,29 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 #include <colorscheme.hpp>
+#include <formatConverter.hpp>
 
 
 int main(int argc, char** argv) {
     std::string imagePath = std::string(argv[1]);
 
     cv::Vec3b colorscheme[] = {
-        cv::Vec3b(26, 27, 38),      // 0: background dark (#1a1b26)
-        cv::Vec3b(36, 40, 59),      // 1: background (#24283b)
-        cv::Vec3b(68, 75, 108),     // 2: comment (#414868)
-        cv::Vec3b(169, 177, 214),   // 3: foreground (#a9b1d6)
-        cv::Vec3b(125, 207, 255),   // 4: cyan (#7dcfff)
-        cv::Vec3b(125, 214, 255),   // 5: blue (#7aa2f7)
-        cv::Vec3b(187, 154, 247),   // 6: purple (#bb9af7)
-        cv::Vec3b(158, 206, 106),   // 7: green (#9ece6a)
-        cv::Vec3b(224, 175, 104),   // 8: yellow (#e0af68)
-        cv::Vec3b(255, 158, 100),   // 9: orange (#ff9e64)
-        cv::Vec3b(247, 118, 142),   // 10: red (#f7768e)
-        cv::Vec3b(255, 117, 127),   // 11: red2 (#ff757f)
-        cv::Vec3b(192, 202, 245),   // 12: blue0 (#c0caf5)
-        cv::Vec3b(115, 218, 202),   // 13: teal (#73daca)
-        cv::Vec3b(217, 224, 238),   // 14: foreground light (#d9e0ee)
-        cv::Vec3b(192, 202, 245)    // 15: terminal white (#c0caf5)
+        FormatConverter::HEXtoRGB("#1a1b26"),   // 0: background dark (#1a1b26)
+        FormatConverter::HEXtoRGB("#24283b"),   // 1: background (#24283b)
+        FormatConverter::HEXtoRGB("#414868"),   // 2: comment (#414868)
+        FormatConverter::HEXtoRGB("#a9b1d6"),   // 3: foreground (#a9b1d6)
+        FormatConverter::HEXtoRGB("#7dcfff"),   // 4: cyan (#7dcfff)
+        FormatConverter::HEXtoRGB("#7aa2f7"),   // 5: blue (#7aa2f7)
+        FormatConverter::HEXtoRGB("#bb9af7"),   // 6: purple (#bb9af7)
+        FormatConverter::HEXtoRGB("#9ece6a"),   // 7: green (#9ece6a)
+        FormatConverter::HEXtoRGB("#e0af68"),   // 8: yellow (#e0af68)
+        FormatConverter::HEXtoRGB("#ff9e64"),   // 9: orange (#ff9e64)
+        FormatConverter::HEXtoRGB("#f7768e"),   // 10: red (#f7768e)
+        FormatConverter::HEXtoRGB("#ff757f"),   // 11: red2 (#ff757f)
+        FormatConverter::HEXtoRGB("#c0caf5"),   // 12: blue0 (#c0caf5)
+        FormatConverter::HEXtoRGB("#73daca"),   // 13: teal (#73daca)
+        FormatConverter::HEXtoRGB("#d9e0ee"),   // 14: foreground light (#d9e0ee)
+        FormatConverter::HEXtoRGB("#c0caf5")   // 15: terminal white (#c0caf5)
     };
     int newWidth = 1366; 
     int newHeight = 768; 
