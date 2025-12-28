4631013c252180d4ea3c6ca81eb8751a27887db6
aappio <alle.appio@gmail.com>
2025-12-28 13:58:37 +0100

    read image with opencv
    Signed-off-by: aappio <alle.appio@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main(){
-    std::cout << "hello world" << std::endl;
+    std::cout << "opening wallpaper.png" << std::endl;
+    cv::Mat image = cv::imread("wallpaper.png");
+    cv::imshow("wallpaper.png", image);
+    cv::waitKey(0);
     return 0;
 }
