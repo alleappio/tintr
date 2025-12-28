b5d08b8f17e04b0241f982a550e7e13c0f25e7a4
aappio <alle.appio@gmail.com>
2025-12-28 21:27:11 +0100

    add command line options
    Signed-off-by: aappio <alle.appio@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,39 @@
 #include <opencv2/opencv.hpp>
 #include <colorscheme.hpp>
 #include <formatConverter.hpp>
+#include <cxxopts.hpp>
 
 int main(int argc, char** argv) {
-    std::string imagePath = std::string(argv[1]);
+    cxxopts::Options options("colorscheme_util", "colorscheme utilities");
+    options.add_options()
+        ("i,input", "path to the input image", cxxopts::value<std::string>())
+        ("c,colorscheme", "path to the colorscheme yaml file", cxxopts::value<std::string>())
+        ("o,output", "path in which the final image will be created", cxxopts::value<std::string>()->default_value("output.png"))
+        ("s,show", "show results in a window", cxxopts::value<bool>())
+        ("h,help", "print usage");
+
+    auto config = options.parse(argc, argv);
+
+    if (config.count("help") || !config.count("input") || !config.count("colorscheme")){
+      std::cout << options.help() << std::endl;
+      exit(0);
+    }
+
+    std::string imagePath = config["input"].as<std::string>();
     int newWidth = 1366; 
     int newHeight = 768; 
 
-    Colorscheme colorscheme("colorschemes/tokyonight.yaml");
+    Colorscheme colorscheme(config["colorscheme"].as<std::string>());
     std::cout << "opening " << imagePath << std::endl;
     cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
     colorscheme.applyToImage(image, image);
-    cv::imwrite("output.png", image);
-    cv::resize(image, image, cv::Size(newWidth, newHeight), cv::INTER_LINEAR);
-    cv::imshow(imagePath, image);
-    cv::waitKey(0);
-    cv::destroyWindow(imagePath);
+    std::cout << "saving image in " << config["output"].as<std::string>() << std::endl;
+    cv::imwrite(config["output"].as<std::string>(), image);
+    if(config["show"].as<bool>()){
+        cv::resize(image, image, cv::Size(newWidth, newHeight), cv::INTER_LINEAR);
+        cv::imshow(imagePath, image);
+        cv::waitKey(0);
+        cv::destroyWindow(imagePath);
+    }
     return 0;
 }
