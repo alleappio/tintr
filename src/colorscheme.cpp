2ffab97fcdeb4aaeddbe1622c99bd863e5f4b3ab
Artem <aeposokhov@gmail.com>
2025-12-30 20:23:46 +0400

    I fixed the way you were handling pallete generation because it crashed, as well as adapted the output format for easy storage as yaml files

--- src/colorscheme.cpp
+++ src/colorscheme.cpp
@@ -1,41 +1,42 @@
 #include <colorscheme.hpp>
 #include <limits>
 
-Colorscheme::Colorscheme(){}
+Colorscheme::Colorscheme() {}
 
-Colorscheme::Colorscheme(std::string filename){
+Colorscheme::Colorscheme(std::string filename) {
     YAML::Node config = YAML::LoadFile(filename);
-    for(unsigned int i=0; i<16; i++){
+    for (unsigned int i = 0; i < 16; i++) {
         std::string hexColor = config["colorscheme"][i].as<std::string>();
         this->colors[i] = FormatConverter::HEXtoRGB(hexColor);
     }
 }
 
-Colorscheme::Colorscheme(cv::Vec3b* colorscheme){
-    for(unsigned int i=0; i<16; i++){
+Colorscheme::Colorscheme(cv::Vec3b *colorscheme) {
+    for (unsigned int i = 0; i < 16; i++) {
         this->colors[i] = colorscheme[i];
     }
 }
 
-Colorscheme::Colorscheme(cv::Mat* image){
+Colorscheme::Colorscheme(cv::Mat *image) {
     this->extractPaletteKMeans(*image, 16);
 }
 
-void Colorscheme::applyToImage(cv::Mat& inImage, cv::Mat& outImage){
-    for(unsigned int i=0; i<inImage.rows; i++){
-        for(unsigned int j=0; j<inImage.cols; j++){
-            cv::Vec3b newColor = this->colors[this->findClosestColor(inImage.at<cv::Vec3b>(i,j))];
-            outImage.at<cv::Vec3b>(i,j) = newColor;
+void Colorscheme::applyToImage(cv::Mat &inImage, cv::Mat &outImage) {
+    for (unsigned int i = 0; i < inImage.rows; i++) {
+        for (unsigned int j = 0; j < inImage.cols; j++) {
+            cv::Vec3b newColor =
+                    this->colors[this->findClosestColor(inImage.at<cv::Vec3b>(i, j))];
+            outImage.at<cv::Vec3b>(i, j) = newColor;
         }
     }
 }
 
-unsigned int Colorscheme::findClosestColor(cv::Vec3b color){
+unsigned int Colorscheme::findClosestColor(cv::Vec3b color) {
     unsigned int result = 0;
-    unsigned int min_distance = 255*3;
-    for(unsigned int i=0; i<16; i++){
+    unsigned int min_distance = 255 * 3;
+    for (unsigned int i = 0; i < 16; i++) {
         unsigned int distance = this->getColorDistance(this->colors[i], color);
-        if(distance<min_distance){
+        if (distance < min_distance) {
             min_distance = distance;
             result = i;
         }
@@ -43,29 +44,30 @@ unsigned int Colorscheme::findClosestColor(cv::Vec3b color){
     return result;
 }
 
-unsigned int Colorscheme::getColorDistance(cv::Vec3b color1, cv::Vec3b color2){
+unsigned int Colorscheme::getColorDistance(cv::Vec3b color1, cv::Vec3b color2) {
     unsigned int distance = 0;
-    for(unsigned int i=0; i<3; i++){
-        distance += abs(color1[i]-color2[i]);
+    for (unsigned int i = 0; i < 3; i++) {
+        distance += abs(color1[i] - color2[i]);
     }
     return distance;
 }
 
-void Colorscheme::extractPaletteKMeans(cv::Mat image, int k){
+void Colorscheme::extractPaletteKMeans(cv::Mat image, int k) {
     cv::Mat data = image.reshape(1, image.rows * image.cols); // Flatten to Nx3
     data.convertTo(data, CV_32F);
-    
+
     cv::Mat labels, centers;
     cv::kmeans(data, k, labels,
-               cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT, 10, 1.0),
-               3, cv::KMEANS_PP_CENTERS, centers);
-    
-    centers.convertTo(centers, CV_8U);
-    for(unsigned int i=0; i<k; i++){
-        this->colors[i] = centers.at<cv::Vec3b>(i);
+                         cv::TermCriteria(cv::TermCriteria::EPS + cv::TermCriteria::COUNT,
+                                                            10, 1.0),
+                         3, cv::KMEANS_PP_CENTERS, centers);
+
+    centers.convertTo(centers, CV_8UC3);
+    for (unsigned int i = 0; i < k; i++) {
+        this->colors[i] =
+                cv::Vec3b(centers.at<uchar>(i, 0), centers.at<uchar>(i, 1),
+                                    centers.at<uchar>(i, 2));
     }
 }
 
-cv::Vec3b* Colorscheme::getColorscheme(){
-    return this->colors;
-}
+cv::Vec3b *Colorscheme::getColorscheme() { return this->colors; }
